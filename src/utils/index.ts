b7fe49a3e948355e2f4e3ba230e5cb0a4cd69d75
danju4rizzl <danjumashiwaju@gmail.com>
2023-03-13 13:25:37 +0200

    removed excess sounds + minor util fix

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,20 +1,16 @@
-import startSound from "../assets/start.mp3"
-import stopSound from "../assets/stop.mp3"
+import notificationSound from "../assets/notificationSound.mp3"
 import music from "../../public/track1.mp3"
 
 
-export const playNotification = (timerStart: boolean) => {
-
-  const audio = new Audio(timerStart ? stopSound : startSound)
+export const playNotification = () => {
+  const audio = new Audio(notificationSound)
   return audio.play()
 }
 
 
-export const formattedTime = (time: number) => {
+export const formatTime = (time: number) => {
   return `${Math.floor(time / 60) < 10
     ? `0${Math.floor(time / 60)}`
     : `${Math.floor(time / 60)}`
     }:${time % 60 < 10 ? `0${time % 60}` : time % 60}`
 }
-
-
